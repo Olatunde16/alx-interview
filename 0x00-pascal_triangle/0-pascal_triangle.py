ee06f5d7d3849c6b24806614375534c498951a77
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-06-27 22:37:27 +0000

    Completed Tasks

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A function that returns a list of lists of integers 
+representing the Pascal’s triangle of n.
+"""
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
