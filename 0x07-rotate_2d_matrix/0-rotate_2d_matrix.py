4ad8f66dc67b8a971b1b9b969677720794fa08d6
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-08-13 06:54:49 +0000

    Updating

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """2D matrix rotation module.
 """
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
-    """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    """Rotates an n x n 2D matrix 90 degrees clockwise in place."""
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            # Swap element at (i, j) with the element at (j, i)
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        # Reverse each row to achieve the 90-degree rotation
+        matrix[i].reverse()
